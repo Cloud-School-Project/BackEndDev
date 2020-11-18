8d713ed4f4c5f64eb2d565b823312abb8de5a9a0
pvahanian <51976194+pvahanian@users.noreply.github.com>
2020-11-18 13:05:59 -0600

    added Cors

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const studentRouter = require('./routers/student-router');
 const volunteerRouter = require('./routers/volunteer-router');
 const classRouter = require('./routers/class-router');
 const adminRouter = require('./admin/admin-router');
-
+const cors = require("cors");
 
 const server = express();
 
-
 server.use(express.json());
+server.use(cors());
 server.use('/student', studentRouter);
 server.use('/volunteer', volunteerRouter);
 server.use('/classes', classRouter);
